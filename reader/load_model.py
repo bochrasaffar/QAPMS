0fb8ee081930ce4e83c9697a9a2f3115bee5fd09
mo5mami <mami.mokhtar123@gmail.com>
2021-05-31 12:47:43 +0100

    some changes

--- reader/load_model.py
+++ reader/load_model.py
@@ -1,3 +1,5 @@
-from transformers import AutoTokenizer, AutoModelForQuestionAnswering
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering , DPRContextEncoder
 tokenizer = AutoTokenizer.from_pretrained("ahmedattia143/roberta_squadv1_base")
 model = AutoModelForQuestionAnswering.from_pretrained("ahmedattia143/roberta_squadv1_base")
+model = DPRContextEncoder.from_pretrained("facebook/dpr-ctx_encoder-single-nq-base")
+model = DPRContextEncoder.from_pretrained("facebook/dpr-question_encoder-single-nq-base")
